424871a61e50dce9bf986ee05d42f0d2e47faa36
Kiran Gundeti <51983300+kirangun@users.noreply.github.com>
2025-04-26 12:09:49 +0530

    Revert "Added Item Reserver logic"

--- src/main/java/com/chtrembl/petstore/order/api/ItemReserveService.java
+++
@@ -1,8 +0,0 @@
-package com.chtrembl.petstore.order.api;
-
-import com.chtrembl.petstore.order.model.Order;
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-public interface ItemReserveService {
-    void saveOrderDataInBlob(Order order) throws JsonProcessingException;
-}
